c1f3b3011957fa2567257b09863b1f4976b85601
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-22 19:30:19 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -9,4 +9,13 @@ class ImageSlider extends Component {
     }
   }
 
- 
+  render() {
+    return (
+      <div>
+        <h1>I am on slide {this.state.currentSlideIndex}</h1>
+      </div>
+    )
+  }
+}
+
+export default ImageSlider
